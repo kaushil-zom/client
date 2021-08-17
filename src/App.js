25a04df589b144e43cc276c5f08dc828eb059a81
Suraj Gupta <suraj.gupta@zomato.com>
2021-08-17 12:48:04 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
 import logo from './logo.svg';
-import {useEffect } from 'react';
 import './App.css';
-import Zoauth2 from 'zoauth';
 
 
 function App() {
-
-  useEffect(() => {
-    const auth = Zoauth2({
-      'client_id': '77eabab1-112e-4527-8b35-b6d894a3f726',
-      'scope': 'offline',
-      'redirect_url': '',
-      'enable_one_tap': false,
-      'logout_url': '',
-      'login_methods': ['google', 'email', 'phone'],
-      'custom': true,
-      'refresh_enable': false,
-    });
-    if (typeof auth.login === "function") {
-      auth.login("IFRAME", "login_ui");
-      // auth.logout();
-      // auth.refresh();
-      // auth.migrate();
-      // setTimeout(function() {auth.initiatePhoneLogin(1, '1123497568', 'sms').then(m => {console.log(m)})}, 1000);
-      // setTimeout(function() {auth.verifyPhoneLogin(1, '1123497568', 123456).then(m => {console.log(m)})}, 1000);
-      // setTimeout(function() {auth.signupPhoneLogin('Suraj', 'def84@def.def').then(m => {console.log(m)})}, 2000);
-      // setTimeout(function() {auth.initiateEmailSignup('gasfla222@agljsf.slgajsl', 'SURAJ').then(m => {console.log(m)})}, 1000);
-      // setTimeout(function() {auth.verifyEmailLogin('gasfla222@agljsf.slgajsl', 'ABCDEF').then(m => {console.log(m)})}, 2000);
-    }
-  }, []);
-
-
   return (
     <div className="App">
       <header className="App-header">
